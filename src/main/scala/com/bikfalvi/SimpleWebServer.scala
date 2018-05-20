69d17e977cd9659bbfff7dd7628251c064c56b7a
Alex Bikfalvi <alex@bikfalvi.com>
2018-05-20 23:49:03 +0200

    Initial commit

---
+++ src/main/scala/com/bikfalvi/SimpleWebServer.scala
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2018 Alex Bikfalvi
+ */
+
+package com.bikfalvi
+
+import com.twitter.finatra.http.HttpServer
+
+class SimpleWebServer extends HttpServer {
+
+}
---
+++ src/main/scala/com/bikfalvi/controllers/LoginController.scala
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2018 Alex Bikfalvi
+ */
+
+package com.bikfalvi.controllers
+
+class LoginController {
+
+}
